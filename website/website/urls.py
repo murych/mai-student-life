45e13d2b17804d2d47fc61ace4c88cbb6a911a6c
winogradoff <winogradoff@gmail.com>
2016-04-13 01:48:13 +0300

    Пути для /about/, /,  community/*

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('communities.urls')),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^$', views.index, name='index'),
+    url(r'^community/', include('community.urls')),
 ]
