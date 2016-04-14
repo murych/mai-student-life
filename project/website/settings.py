685768628efc9ffa3f1721a54c1b8985fd046052
winogradoff <winogradoff@gmail.com>
2016-04-14 03:40:02 +0300

    PROJECT_DIR -> BASE_DIR для static и media.

--- project/website/settings.py
+++ project/website/settings.py
@@ -119,11 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(__file__)
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, "static"),
+    os.path.join(BASE_DIR, "static"),
 )
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
