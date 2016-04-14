bdbc0f3a4cdcecd8f544e7dcbd4f71edf12a5ec9
winogradoff <winogradoff@gmail.com>
2016-04-14 03:54:02 +0300

    views.py перенесен из website в common.

--- project/common/views.py
+++ project/common/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+
+
+def index(request):
+    return redirect(reverse('community:list'))
+
+
+def about(request):
+    template_name = 'common/about.html'
+    return render(request, template_name)
