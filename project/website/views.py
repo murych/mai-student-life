e54b0839e37dd4881ac4a1a9e25f229679d930db
Euphe <euphetar@gmail.com>
2016-05-12 17:59:58 +0300

    remove rudimentary

--- project/website/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-
-# Create your views here.
-
-def about(request):
-    template_name = 'about.html'
-    return render_to_response(template_name, context_instance=RequestContext(request))
